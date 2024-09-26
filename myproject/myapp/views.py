65423a35579e0adc03b6420117bea62455b66a06
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-26 13:06:03 -0700

    Update views.py

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -5,3 +5,5 @@ from django.http import HttpResponse
 # Create your views here.
 def menu(request):
     menu_items = Menu.objects.all()
+    items_dict = {"menu":menu_items}
+    return render(request, "menu.html", items_dict)
