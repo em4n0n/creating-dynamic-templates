2629cfd727472ec0f4fcf4a3ea0d69903c661b05
em4n0n <58372945+em4n0n@users.noreply.github.com>
2024-09-28 14:16:52 -0700

    Register menu function in urls.py

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from .models import Menu
 from django.http import HttpResponse
 
-# Create your views here.
 def menu(request):
-    menu_items = Menu.objects.all().order_by('name')
+    menu_items = Menu.objects.all()
     items_dict = {"menu":menu_items}
     return render(request, "menu.html", items_dict)
